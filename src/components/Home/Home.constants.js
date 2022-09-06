af093c9a92bb9fb3da675152e11d2de61749a8ca
harishnanthan <h.chetta17@gmail.com>
2022-09-06 23:17:41 +0530

    change some ui design

--- src/components/Home/Home.constants.js
+++ src/components/Home/Home.constants.js
@@ -1,6 +1,6 @@
 import { timeCalculation } from "./Home.helper"
 
-export const headingText = `Product Designer creating thoughtful, intuitive interfaces.`
+export const headingText = `Creative React js developer, Website designer.`
 export const subHeadingText = `I’m Harish Nanthan, a Reactjs developer with over ${timeCalculation()} of experience. I specialise in interface design for mobile and web-based applications with a focus on simplicity & usability.`
 export const companyName = `Tekion`
-export const introsubText = `I’m currently working at ${companyName} on some of the worlds best product based startup. Before that, I worked at BaseKit where I helped shape the future of website builders and tools to help small businesses thrive online. I also build tools like Design Vault and Does.Design.`
+export const introsubText = `I’m currently working at ${companyName} on some of the worlds best product based startup. Before that, I worked some college projects where I helped shape the future of website builders and tools to help small businesses thrive online. I also do simple design and make websites responsive.`
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,9 @@ import { NavLink } from "react-router-dom"
 function Navigation() {
   return (
     <div className="navigation_wrapper">
-        <div className="navigation_wrapper__left"></div>
+        <div className="navigation_wrapper__left">
+          Harish Nanthan
+        </div>
         <div className="navigation_wrapper__right">
             <ul className="navigation_wrapper__right___links">
                 <NavLink to='/'>Home</NavLink>
